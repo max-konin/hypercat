0857f5a72b6bbe4474cceace7311cd1e859cd953
max <maxim21214@gmail.com>
2016-01-27 12:37:53 +0600

    singular type in json api

--- config/application.rb
+++ config/application.rb
@@ -30,5 +30,6 @@ module Hypercat
     # config.i18n.default_locale = :de
     config.react.addons = true
     ActiveModel::Serializer.config.adapter = :json_api
+    ActiveModel::Serializer.config.jsonapi_resource_type = :singular
   end
 end
