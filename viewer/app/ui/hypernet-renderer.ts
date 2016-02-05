b7868611406a10da089d134655542fab205cba07
Eduard Y. Lepner <elepner@dataeast.com>
2016-02-05 17:20:02 +0600

    Added refreshing of edges while dragging the node.

--- viewer/app/ui/hypernet-renderer.ts
+++ viewer/app/ui/hypernet-renderer.ts
@@ -1,30 +1,50 @@
 import {GraphComponents} from '../core/graph'
+import {EventListener} from '../core/event-listener'
 export class HyperNetRenderer {
     private graph: GraphComponents.Graph;
     private canvas: HypernetCanvas;
+    private map: Map<GraphComponents.Vertex, CanvasPoint>;
+    private edgeMap: Map<GraphComponents.Edge, CanvasEdge>;
     constructor(graph: GraphComponents.Graph, hypernetCanvas: HypernetCanvas) {
         this.graph = graph;
         this.canvas = hypernetCanvas;
         this.canvas.setMaxExtent(0, 4, 0, 4);
+        this.map = new Map<GraphComponents.Vertex, CanvasPoint>();
+        this.edgeMap = new Map<GraphComponents.Edge, CanvasEdge>();
     }
 
     public draw() {
-        this.graph.edges.forEach((edge) => {
-            debugger;
-            this.canvas.drawLine(
-                { x: edge.from.x, y: edge.from.y },
-                { x: edge.to.x, y: edge.to.y });
-        });
         
         this.graph.vertices.forEach((vertex) => {
-            this.canvas.drawPoint({x : vertex.x, y : vertex.y});
-        })
+            var point = new CanvasPoint(vertex);
+            point.VertexUpdatedListener = (canvasPoint) => {
+                this.graph.getAdjacent(canvasPoint.Vertex).forEach((edge) => {
+                    var canvasEdge = this.edgeMap.get(edge);
+                    if(canvasEdge.EdgeRefreshListener){
+                        canvasEdge.EdgeRefreshListener(canvasEdge);
+                    }
+                });
+            }
+            this.map.set(vertex, point);
+        });
+        
+        this.graph.edges.forEach((edge) => {
+            var from = this.map.get(edge.from);
+            var to = this.map.get(edge.to);
+            var canvasEdge = new CanvasEdge(edge, from, to);
+            this.edgeMap.set(edge, canvasEdge);
+            this.canvas.drawLine(canvasEdge);
+        });
+        
+        this.map.forEach((vertex) => {
+            this.canvas.drawPoint(vertex);
+        });
     }
 }
 
 export interface HypernetCanvas {
-    drawLine(from: Point, to: Point);
-    drawPoint(point: Point);
+    drawLine(edge: CanvasEdge);
+    drawPoint(point: CanvasPoint);
     setMaxExtent(xmin: number, xmax: number, ymin: number, ymax: number);
 }
 
@@ -32,3 +52,61 @@ export class Point {
     x: number;
     y: number;
 }
+
+export class CanvasEdge {
+    private fromPt: CanvasPoint;
+    private toPt: CanvasPoint;
+    private _edge;
+    public EdgeRefreshListener: EventListener<CanvasEdge>
+    public data: any;
+    
+    constructor (edge: GraphComponents.Edge, from: CanvasPoint, to: CanvasPoint){
+        this.fromPt = from;
+        this.toPt = to;
+        this._edge = edge;
+    }
+    
+    public get from() : CanvasPoint {
+        return this.fromPt;
+    }
+    
+    public get to() : CanvasPoint {
+        return this.toPt;
+    }
+    
+    public get edge() : GraphComponents.Edge {
+        return this._edge;
+    }
+}
+
+export class CanvasPoint {
+    private vertex: GraphComponents.Vertex;
+    
+    public VertexUpdatedListener: EventListener<CanvasPoint>
+    
+    constructor (vertex: GraphComponents.Vertex){
+        this.vertex = vertex;
+    }
+    
+    public get x() : number {
+        return this.vertex.x;
+    }
+    
+    public get y() : number {
+        return this.vertex.y;
+    }
+    
+    public get Vertex() : GraphComponents.Vertex {
+        return this.vertex;
+    }
+    
+    public setCoordinates(x: number, y: number){
+        this.vertex.x = x;
+        this.vertex.y = y;
+        if(this.VertexUpdatedListener){
+            this.VertexUpdatedListener(this);
+        }
+    }
+}
+
+
