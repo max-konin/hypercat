968f3b031e23ae64d44f803d29f9b1c9cc11d23e
max <maxim21214@gmail.com>
2016-03-31 23:04:02 +0600

    ready to deploy

--- viewer/ember-cli-build.js
+++ viewer/ember-cli-build.js
@@ -5,8 +5,7 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
     fingerprint: {
-      prepend: '/assets/ember/',
-      extensions: ['js', 'css', 'png', 'gif', 'jpeg', 'jpg', 'svg', 'eot', 'ttf', 'woff']
+      prepend: '/assets/ember/'
     }
   });
 
